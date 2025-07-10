d521c04821a381c99e7e67ba27888c7d818d6583
Kavya-kriti <kritikavya65@gmail.com>
2025-07-10 14:14:58 +0530

    Add files via upload

---
+++ CodeAlpha_Calculator/script.js
@@ -0,0 +1,31 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  const key = e.key;
+
+  if ("0123456789+-*/().".includes(key)) {
+    appendValue(key);
+  } else if (key === "Enter") {
+    calculate();
+  } else if (key === "Backspace") {
+    display.value = display.value.slice(0, -1);
+  } else if (key === "Escape") {
+    clearDisplay();
+  }
+});
